6c471461f0cd66fc73a0394adbd239cc0758c47e
samule404 <59326532+samule404@users.noreply.github.com>
2022-10-14 11:00:27 +0800

    Update GitTest.java

--- src/main/java/com/sjun/git/GitTest.java
+++ src/main/java/com/sjun/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("hello git add by hot-fix!");
         System.out.println("master test");
         System.out.println("hot-fix test");
-        System.out.printlin("pull test");
+        System.out.println("pull test");
     }
 }
