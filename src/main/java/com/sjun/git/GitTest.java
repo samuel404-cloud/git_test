b5edcd702b25c933bf2e62842fa53d43d9489855
samule404 <59326532+samule404@users.noreply.github.com>
2022-10-14 10:56:48 +0800

    pulll commit

--- src/main/java/com/sjun/git/GitTest.java
+++ src/main/java/com/sjun/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git add by hot-fix!");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.printlin("pull test");
     }
 }
