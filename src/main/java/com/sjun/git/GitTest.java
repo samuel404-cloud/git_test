657d89b2d3fc723d43656ea65001cdb0699abc62
samuel <1928737694@qq.com>
2022-10-14 09:21:00 +0800

    second git_demo

--- src/main/java/com/sjun/git/GitTest.java
+++ src/main/java/com/sjun/git/GitTest.java
@@ -3,5 +3,6 @@ package com.sjun.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
